49c9d1d96a115746e07ab2d6d7316846be758260
bambi <samug14@gmail.com>
2021-08-16 22:53:26 +0100

    generate template

--- generateSheet.py
+++ generateSheet.py
@@ -23,3 +23,93 @@ def generateScore(time, staffs):
     >>
 {CB}
     """
+def makeBase():
+	"""
+	celloOne = \\relative c {OB}
+	
+		{CB}
+	
+	celloTwo = \\relative c {OB}
+	
+		{CB}
+	
+	global = {OB}
+		tempo = 3/4
+		key = b \\major
+	{CB}
+	
+	
+	\\book {OB}
+		\\paper {OB}
+			print-all-headers = ##t
+			print-page-number = ##f
+		{CB}
+		\\score {OB}
+			<<
+				\\new Staff \\with {OB}
+					instrument = #"Violoncello 1"
+				{CB}
+				{OB}
+					\\global
+					\\celloOne
+				{CB}
+				\\new Staff \\with {OB}
+		                        instrumentName = #"Violoncello 2"
+		                {CB}
+				{OB}
+					\\global
+					\\celloTwo
+				{CB}
+			>>
+		\\header {OB}
+			title = "Sous le dôme épais"
+			subtitle = "for two cellos"
+			composer = "Léo Delibes"
+			arranger = "Krystian Figiel"
+			{CB}
+		{CB}
+		\\pageBreak
+		
+		\\score {OB}
+			<<
+				\\new Staff \\with
+				{OB}
+					instrumentName = #"Violoncello 1"
+				{CB}
+				{OB}
+					\\global
+					\\celloOne
+				{CB}
+			>>
+			\\header {OB}
+			title = "Sous le dôme épais"
+			subtitle = "for two cellos"
+			composer = "Léo Delibes"
+			arranger = "Krystian Figiel"
+			{CB}	
+			
+		{CB}
+		\\pageBreak
+		\\score {OB}
+			<<
+				\\new Staff \\with
+				{OB}
+					instrumentName = #"Violoncello 2"
+				{CB}
+				{OB}
+					\\global
+					\\celloTwo
+				{CB}
+			>>
+			\\header {OB}
+			title = "Sous le dôme épais"
+			subtitle = "for two cellos"
+			composer = "Léo Delibes"
+			arranger = "Krystian Figiel"
+			{CB}
+	
+		{CB}
+	{CB}
+	
+	\\version "2.18.2"
+"""
