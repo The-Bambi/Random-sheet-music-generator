0c88626a25f3330d8baed84337c1029ffa14c697
bambi <samug14@gmail.com>
2021-12-14 17:29:18 +0000

    quick doc coz I forgot

--- generateSheet.py
+++ generateSheet.py
@@ -23,93 +23,82 @@ def generateScore(time, staffs):
     >>
 {CB}
     """
-def makeBase():
-	"""
-	celloOne = \\relative c {OB}
+def makeBase(variables, voices, title, subtitle, composer, time, key):
+    """
+    variables - list of names of variables in the codes for each voice line
+    voices - instruments, like ['Violin 1', 'Violin 2', 'blah']
+    title - self-expl.
+    subtitle - kappa
+    composer - kappa
+    time kappa
+    key kappa
+    """
+    voices_str = ""
+    staffs_str = ""
+    scores_str = ""
+
+
+    header = f"""\\header {OB}
+        title = {title}
+        subtitle = {subtitle}
+        composer = {composer}
+        arranger = "Krystian Figiel"
+        {CB}"""
+
+    for voice, variable in zip(voices, variables):
+        voice_str = f"""
+        {variable} = \\relative c {OB}
+
+        {CB}
+        """
+        voices_str += voice_str
+
+        staff_str = f"""
+        \\new Staff \\with {OB}
+            instrumentName = #"{voice}"
+        {CB}
+        {OB}
+            \\global
+            \\{variable}
+        {CB}
+        """
+        staffs_str += staff_str
+
+        score_str = f"""
+        \\score {OB}
+                <<
+                    {staff_str}
+                >>
+                {header}
+                {CB}
+        \\pageBreak
+        """
+        scores_str += score_str
+
+    return f"""
+    
+    {voices_str}
+
+    global = {OB}
+	    \\time {time}
+	    \\key {key}
+    {CB}
+    
+    
+    \\book {OB}
+	    \\paper {OB}
+		    print-all-headers = ##t
+		    print-page-number = ##f
+	    {CB}
+	    \\score {OB}
+		    <<
+                        {staffs_str}
+		    >>
+	    {header}
+	    {CB}
+	    \\pageBreak
 	
-		{CB}
-	
-	celloTwo = \\relative c {OB}
-	
-		{CB}
-	
-	global = {OB}
-		tempo = 3/4
-		key = b \\major
-	{CB}
-	
-	
-	\\book {OB}
-		\\paper {OB}
-			print-all-headers = ##t
-			print-page-number = ##f
-		{CB}
-		\\score {OB}
-			<<
-				\\new Staff \\with {OB}
-					instrument = #"Violoncello 1"
-				{CB}
-				{OB}
-					\\global
-					\\celloOne
-				{CB}
-				\\new Staff \\with {OB}
-		                        instrumentName = #"Violoncello 2"
-		                {CB}
-				{OB}
-					\\global
-					\\celloTwo
-				{CB}
-			>>
-		\\header {OB}
-			title = "Sous le dôme épais"
-			subtitle = "for two cellos"
-			composer = "Léo Delibes"
-			arranger = "Krystian Figiel"
-			{CB}
-		{CB}
-		\\pageBreak
-		
-		\\score {OB}
-			<<
-				\\new Staff \\with
-				{OB}
-					instrumentName = #"Violoncello 1"
-				{CB}
-				{OB}
-					\\global
-					\\celloOne
-				{CB}
-			>>
-			\\header {OB}
-			title = "Sous le dôme épais"
-			subtitle = "for two cellos"
-			composer = "Léo Delibes"
-			arranger = "Krystian Figiel"
-			{CB}	
-			
-		{CB}
-		\\pageBreak
-		\\score {OB}
-			<<
-				\\new Staff \\with
-				{OB}
-					instrumentName = #"Violoncello 2"
-				{CB}
-				{OB}
-					\\global
-					\\celloTwo
-				{CB}
-			>>
-			\\header {OB}
-			title = "Sous le dôme épais"
-			subtitle = "for two cellos"
-			composer = "Léo Delibes"
-			arranger = "Krystian Figiel"
-			{CB}
-	
-		{CB}
-	{CB}
-	
-	\\version "2.18.2"
+        {scores_str}    
+    {CB}
+    \\version "2.18.2"
 """
